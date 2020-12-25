033a58b602de598b4430ba26e453b77502570670
thegenggo <kritsanaphong-keang@hotmail.com>
2020-12-25 16:03:03 +0700

    lab6_3 updated

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x=='A')
+	{
+		return 'Z';
+	}
+	else if(x>'A'&& x<'Z')
+	{
+		return x-1;
+	}
+	else if(x=='Z')
+	{
+		return 'A';
+	}
+	else
+	{
+		return '0';
+	}
 }
 
 int main(){
